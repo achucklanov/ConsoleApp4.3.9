c9db6a73cd4346fe3456d1907643bcb3452db96f
AndreyChuck <a.chucklanov@yandex.ru>
2023-03-12 22:10:34 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp4.3.9/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp4._3._9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = { { 1, 2, 3, }, { 5, 6, 7 } };
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
